9e675f6b60b02596fdfc6fa27155ae510822fa25
raffaelananda17 <168395782+raffaelananda17@users.noreply.github.com>
2024-04-29 12:05:02 +0700

    raffaelananda-backend2

--- index.js
+++ index.js
@@ -1,22 +1,68 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8080
 
-app.get('/', (req, res) =>{
-    res.send('Menyala Abangkuhh')
+app.get('/mahasiswa/:nim', (req,res)=>{
+    const nim = req.params.nim
+
+    res.send(`Mahasiswa dengan nim ${nim} di temukan`)
 });
 
-app.post ('/', (req, res)=>{
-    res.send('post data')
+
+app.get('/mahasiswa/:nim/:semester', (req,res)=>{
+
+    const nim = req.params.nim
+    const semester = req.params.semester
+
+    res.send(`Mahasiswa dengan : ${nim} semester :${semester} di temukan`)
+});
+
+
+app.get('/get-mahasiswa-by-nim', (req, res)=>{
+    const nim = req.query.nim
+
+    res.send(`Mahasiswa dengan nim${nim} di temukan`)
+});
+
+app.get('/nilai-persemester', (req,res)=>{
+    const nim = req.query.nim
+    const semester = req.query.semester
+
+    res.send(`Mahasiswa dengan nim : ${nim} semester : ${semester} di temukan`)
 })
 
-app.put ('/', (req, res)=>{
-    res.send('upload sucsesful')
+app.use(express.json()),
+
+app.post('/mahasiswa', (req,res)=>{
+    const nim = req.body.nim;
+    const nama = req.body.nama;
+    const angkatan = req.body.angkatan;
+    const prodi = req.body.prodi;
+
+    const msg = {status:"sukses",
+                    data:{"nim" : nim, "nama" : nama, "angkatan" : angkatan, "prodi" : prodi}};
+    res.send(msg);
 })
 
-app.delete ('/', (req, res)=>{
-    res.send('delete sucsesful')
-})
+
+
+
+
+// app.get('/', (req, res) =>{
+//     res.send('Menyala Abangkuhh')
+// });
+
+// app.post ('/', (req, res)=>{
+//     res.send('post data')
+// })
+
+// app.put ('/', (req, res)=>{
+//     res.send('upload sucsesful')
+// })
+
+// app.delete ('/', (req, res)=>{
+//     res.send('delete sucsesful')
+// })
 
 app.listen(port, () =>{
     console.log(`server berjalan pada localhost:${port}`)
